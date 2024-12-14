e011b7f7c71bbe061b0ca82fec8efcd542ceeb11
GuestyTheBesty <guestythebesty841@gmail.com>
2024-12-14 07:41:44 -0800

    hi edf

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+import sqlite3
 
 app = Flask(__name__)
 
@@ -7,6 +8,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+con = sqlite3.connect("downlaod.db", check_same_thread=False)
+cur = con.cursor()
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-  ...
+  if request.method == "POST":
+    ...
+  return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
